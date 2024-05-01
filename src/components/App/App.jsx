994d53515db3c8488f91576c04dcf16283fc4d47
EZH1809 <stellage.ua@gmail.com>
2024-05-01 19:14:12 +0300

    add start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react';
+
+import './App.module.css';
+
+ export default function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+
